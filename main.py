23e7a415ef3257ee25c8056aade6dd12c8e92f75
Lukas van Driel <lukas@q42.nl>
2015-01-14 16:36:37 +0100

    output json instead of txt

--- main.py
+++ main.py
@@ -2,10 +2,13 @@
 #
 
 import webapp2
+import json
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        stuff = ['foo', {'bar': ('baz', None, 1.0, 2)}]
+        self.response.content_type = 'text/json'
+        self.response.write(json.dumps(stuff))
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
